f64e30a16b881f9288e33a5aa315409d053ed3a5
v.ostapov <v.ttbomk@gmail.com>
2022-10-09 16:43:00 +0300

    Add Map

--- src/app.js
+++ src/app.js
@@ -3,12 +3,29 @@ import 'bootstrap'
 // import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
 import menu from './assets/scripts/menu';
 import sliders from "./assets/scripts/sliders";
+import { initMaps } from './assets/scripts/modules/map/main';
+import Splitting from './assets/scripts/vendor/splitting';
+import sal from './assets/scripts/vendor/sal';
+
+window.Splitting = Splitting;
+window.sal = sal;
 
 // Styles
 import 'styles/_app.scss'
 // import 'simplebar/dist/simplebar.css';
 
+window.addEventListener('DOMContentLoaded', () => {
+  // Utils
+  // ---------------------------------
 
+  // Modules
+  // ---------------------------------
+  initMaps();
 
-sliders.init();
-menu.init();
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    sliders.init();
+    menu.init();
+  });
+});
